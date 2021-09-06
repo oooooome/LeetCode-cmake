d55dda90143c820ae79349f898dad7e0458c5def
liruijie <liruijie@ebupt.com>
2021-09-06 23:06:20 +0800

    1

--- solution1.cpp
+++ solution1.cpp
@@ -1,12 +1,37 @@
-#include "stdio.h"
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
+class Solution1 {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int i1 = 0, i2 = 0;
+		int l = nums.size();
+		for(i1 = 0; i1< l; ++i1)
+		{
+			for(i2 = 0; i2 < l; ++i2)
+			{
+				if (nums[i1] + nums[i2] == target && i1 != i2)
+				{
+					return vector<int>{i1, i2};
+				}
+			}
+			
+			
+		}
 
+		return vector<int>{-1, -1};
+	}
+};
 
 
 int main()
 {
-	printf("hello solution1");
-
+	vector<int> nums{ 3,2,4 };
+	int target = 6;
+	Solution1 s{};
+	auto ret = s.twoSum(nums, target);
+	cout << ret[0] << " " << ret[1];
 	return 0;
 }
